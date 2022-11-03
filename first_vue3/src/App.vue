74651e9dfd83d2e27a7786238acc885527bdb7c0
rgirodon <remy.girodon@gmail.com>
2022-11-03 15:45:15 +0100

    Initial commit

---
+++ first_vue3/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+</style>
