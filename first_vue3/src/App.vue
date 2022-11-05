d4ab4a26beedfb54f42085a8ed6c28275f465e10
rgirodon <remy.girodon@gmail.com>
2022-11-05 10:09:24 +0100

    Set conditional display

--- first_vue3/src/App.vue
+++ first_vue3/src/App.vue
@@ -4,12 +4,21 @@ export default {
     return {
       count: 0
     }
+  },
+  methods: {
+    isLifeAwesome() {
+      return (Math.floor(Math.random() * 100) % 2 == 0);
+    }
   }
 }
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div>  
+    <h1 v-if="isLifeAwesome()">Life is awesome !</h1>
+    <h1 v-else>Life is so sad !</h1>
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
 </template>
 
 <style scoped>
