f1f6bc183df911781d254dea686e40eb0950f730
rgirodon <remy.girodon@gmail.com>
2022-11-05 10:00:42 +0100

    Set computed property and method for reversed message

--- first_vue3/src/App.vue
+++ first_vue3/src/App.vue
@@ -1,16 +1,37 @@
 <script>
 export default {
+  
   data() {
     return {
-      count: 0
+      count: 0,
+      message: "Hello"
+    }
+  },
+
+  computed : {
+    reversedMessage() {
+      return this.reverseString(this.message);
+    }    
+  },
+
+  methods : {
+    reverseString(str) {
+      return str.split("").reverse().join("");
     }
   }
 }
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div>
+    <button @click="count++">Count is: {{ count }}</button>
+    <p>{{ message }}</p>
+    <p class="reversed">{{ reversedMessage }}</p>
+  </div>
 </template>
 
 <style scoped>
+p.reversed {
+  font-style: italic;
+}
 </style>
