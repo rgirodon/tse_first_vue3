72dc0784be7b010a8c10136f9f8d858d54d4605f
rgirodon <remy.girodon@gmail.com>
2022-11-05 10:13:52 +0100

    Iterative display

--- first_vue3/src/App.vue
+++ first_vue3/src/App.vue
@@ -2,14 +2,20 @@
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      shoppingList: [{ key:1, article: 'Lessive' }, { key:2, article: 'Lait' }]
     }
   }
 }
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div>
+    <button @click="count++">Count is: {{ count }}</button>
+    <ul>
+      <li v-for="item in shoppingList" :key="item.id">{{ item.article }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
