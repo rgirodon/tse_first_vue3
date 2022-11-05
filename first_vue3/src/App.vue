2b7b567331da642d8d30e4a018fdc123113cbf7a
rgirodon <remy.girodon@gmail.com>
2022-11-05 10:24:47 +0100

    set up form

--- first_vue3/src/App.vue
+++ first_vue3/src/App.vue
@@ -2,14 +2,29 @@
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      name: "",
+      displayedName: "" 
+    }
+  },
+  methods: {
+    copyNameToDisplayedName() {
+      this.displayedName = this.name;
     }
   }
 }
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div>
+    <button @click="count++">Count is: {{ count }}</button>
+
+    <div>
+      <input v-model="name">
+      <button @click="copyNameToDisplayedName">Reveal</button>
+      <p>You are : {{ displayedName }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
