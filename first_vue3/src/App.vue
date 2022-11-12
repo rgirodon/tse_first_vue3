dcfaf94636e652a394faa601febc220bd77ca2bb
rgirodon <remy.girodon@gmail.com>
2022-11-12 10:14:10 +0100

    Add counter component

--- first_vue3/src/App.vue
+++ first_vue3/src/App.vue
@@ -1,15 +1,23 @@
 <script>
+import CounterComponent from './components/CounterComponent.vue';
+
 export default {
   data() {
     return {
       message: "Hello TSE !"
     }
+  },
+  components: {
+    CounterComponent
   }
 }
 </script>
 
 <template>
   <p>The message : {{ message }}</p>
+  <counter-component :initial="0" :increment="1" />
+  <counter-component :initial="0" :increment="2" />
+  <counter-component :initial="100" :increment="-1" />
 </template>
 
 <style scoped>
---
+++ first_vue3/src/components/CounterComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        initial: Number,
+        increment: Number
+    },
+    data() {
+        return {
+            count: 0
+        }
+    },
+    created: function() {
+        this.count = this.initial;
+    },
+    methods: {
+        incrementHandler() {
+            this.count += this.increment;
+        }
+    }
+}
+</script>
+
+<template>
+  <button @click="incrementHandler">You clicked me {{ count }} times.</button>
+</template>
